2fb7a8659e7814736c219b9ef5b9ae1c8cb62eb4
uhablog <uha.blog98@gmail.com>
2024-05-15 17:33:18 +0900

    Todoは複数のサブTodoを持てるようになった

--- src/main.py
+++ src/main.py
@@ -1,21 +1,31 @@
 from typing import List
+from datetime import datetime
+import uuid
+import enum
 
 import strawberry
 from fastapi import FastAPI
 from strawberry.asgi import GraphQL
+from strawberry.scalars import ID
+
+@strawberry.enum
+class Priority(enum.Enum):
+    HIGH = "高い"
+    MEDIUM = "中くらい"
+    LOW = "低い"
 
 @strawberry.type
 class SubTodo:
+    id: ID
     subtitle: str
 
 @strawberry.type
 class Todo:
-    id: int
+    id: ID
     title: str
-    subTodo: List[SubTodo]
-    # limit: Datetime
-    # priority: str
-    # user_id: str
+    limit: datetime
+    priority: Priority
+    subTodos: List[SubTodo] = strawberry.field(default_factory=list)
 
 todos: List[Todo] = []
 
@@ -30,14 +40,29 @@ class Query:
 class Mutation:
 
     @strawberry.mutation
-    def create_todo(self, title: str) -> Todo:
-        new_todo = Todo(id=len(todos)+1, title=title, subTodo=[])
+    def create_todo(self, title: str,
+                    limit: datetime,
+                    priority: Priority
+                    ) -> Todo:
+        new_todo = Todo(
+                    id=strawberry.ID(str(uuid.uuid4())),
+                    title=title,
+                    limit=limit,
+                    priority=priority
+                   )
         todos.append(new_todo)
         return new_todo
 
     @strawberry.mutation
-    def create_sub_todo(self, subtitle: str) -> SubTodo:
-        new_sub_todo = SubTodo(subtitle=subtitle)
+    def create_sub_todo(self, todo_id: strawberry.ID, subtitle: str) -> SubTodo:
+
+        for todo in todos:
+            if todo.id == todo_id:
+                new_sub_todo = SubTodo(id=strawberry.ID(str(uuid.uuid4())), subtitle=subtitle)
+                todo.subTodos.append(new_sub_todo)
+                return new_sub_todo
+
+        raise ValueError(f"Todo with ID {todo_id} not found")
 
 schema = strawberry.Schema(query=Query, mutation=Mutation)
 
